eb970b7d6b28747938ed0b8d3bf886e47adf6c46
ChrisSwingle <swingle.chris@gmail.com>
2024-07-30 20:10:52 -0400

    basic auth login right out the box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:like_minds/amplifyconfiguration.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:like_minds/screens/home_screen.dart';
 
@@ -12,23 +18,42 @@ final theme = ThemeData(
   textTheme: GoogleFonts.frauncesTextTheme(),
 );
 
-void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await _configureAmplify();
+    runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+  } on AmplifyException catch (e) {
+    runApp(Text("Error configuring Amplify: ${e.message}"));
+  }
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    await Amplify.addPlugin(AmplifyAuthCognito());
+    await Amplify.configure(amplifyconfig);
+    safePrint('Successfully configured');
+  } on Exception catch (e) {
+    safePrint('Error configuring Amplify: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: theme,
-      home: const HomeScreen(),
+    return Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        title: 'Like Minds',
+        theme: theme,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
